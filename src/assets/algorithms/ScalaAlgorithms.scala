e57f3c638742cd874f911521b9ddccf3d7de6e11
Luis Arce <luis.arce22@gmail.com>
2019-01-21 14:18:55 -0400

    Quick sort scala algorithm

--- src/assets/algorithms/ScalaAlgorithms.scala
+++ src/assets/algorithms/ScalaAlgorithms.scala
@@ -16,6 +16,18 @@ object ScalaAlgorithms {
     if (flag) Bluesort(array)
     else array
   }
+
+  def quickSort(xs: Array[Int]): Array[Int] = {
+    if (xs.length <= 1) xs
+    else {
+      val pivot = xs(xs.length / 2)
+      Array.concat(
+        quickSort(xs filter (pivot >)),
+        xs filter (pivot ==),
+        quickSort(xs filter (pivot <))
+      )
+    }
+  }
   
 }
 
